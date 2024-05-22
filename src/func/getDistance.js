e0d2af56c84817a1d1655caf2132592b7e55f2ae
schentea <schentea@gmail.com>
2024-05-22 15:49:48 +0900

    a

--- src/func/getDistance.js
+++ src/func/getDistance.js
@@ -14,7 +14,7 @@ export const getDistanceBetweenPoints = (lat1, lon1, lat2, lon2) => {
     Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-  const distance = R * c;
+  let distance = R * c;
   if(distance < 1) {
     distance = 1;
   }
