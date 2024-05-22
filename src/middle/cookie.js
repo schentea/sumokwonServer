91024dd702d53fb5f0757ce4aff48a0b296df2ce
schentea <schentea@gmail.com>
2024-05-22 11:26:33 +0900

    a

--- src/middle/cookie.js
+++ src/middle/cookie.js
@@ -12,7 +12,7 @@ cookieMiddleware.setCookie = (req, res, next) => {
 
 cookieMiddleware.readCookies = (req,res,next) => {
   const cookies =req.cookies;
-  console.log("받은 쿠키",cookies)
+  console.log(`받은 쿠키: ${cookies}`, typeof(cookies))
   const myCookie = cookies.myCookieName;
   console.log("쿠키 이름",myCookie)
   next()
