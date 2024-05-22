e81d773fd1c74dd7c66c07e7bd05f716684bb661
schentea <schentea@gmail.com>
2024-05-22 11:31:34 +0900

    a

--- src/middle/cookie.js
+++ src/middle/cookie.js
@@ -1,21 +1,22 @@
-const cookieMiddleware = {}
+const cookieMiddleware = {};
 
 cookieMiddleware.setCookie = (req, res, next) => {
-  res.cookie('myCookieName','cookieValue', {
-    maxAge:900000, 
-    httpOnly:true,
-    domain : 'port-0-sumokwonserver-17xco2nlstnj7hw.sel5.cloudtype.app',
-    path : '/'
-  });
-  next()
+    res.cookie('myCookieName', 'cookieValue', {
+        maxAge: 900000,
+        httpOnly: true,
+        domain: 'port-0-sumokwonserver-17xco2nlstnj7hw.sel5.cloudtype.app',
+        path: '/'
+    });
+    next();
 };
 
-cookieMiddleware.readCookies = (req,res,next) => {
-  const cookies =req.cookies;
-  console.log(`받은 쿠키: ${cookies}`, typeof(cookies))
-  const myCookie = cookies.myCookieName;
-  console.log("쿠키 이름",myCookie)
-  next()
-}
+cookieMiddleware.readCookies = (req, res, next) => {
+    const cookies = req.cookies;
+    console.log("받은 쿠키:", cookies);
+    console.log("쿠키 타입:", typeof cookies);
+    const myCookie = cookies.myCookieName;
+    console.log("쿠키 이름:", myCookie);
+    next();
+};
 
-module.exports = cookieMiddleware
+module.exports = cookieMiddleware;
