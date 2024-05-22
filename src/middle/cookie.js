179ba602b6abc776b57b885cf4f3654b4ac7c111
schentea <schentea@gmail.com>
2024-05-22 11:45:55 +0900

    a

--- src/middle/cookie.js
+++ src/middle/cookie.js
@@ -1,7 +1,7 @@
 const cookieMiddleware = {};
 
 cookieMiddleware.setCookie = (req, res, next) => {
-    res.cookie('myCookieName', 'cookieValue', {
+    res.cookie('name', 'cookieValue', {
         maxAge: 900000,
         httpOnly: true,
         domain: 'port-0-sumokwonserver-17xco2nlstnj7hw.sel5.cloudtype.app',
@@ -14,7 +14,7 @@ cookieMiddleware.readCookies = (req, res, next) => {
     const cookies = req.cookies;
     console.log("받은 쿠키:", cookies);
     console.log("쿠키 타입:", typeof cookies);
-    const myCookie = cookies.myCookieName;
+    const myCookie = cookies.name; // 클라이언트에서 설정한 쿠키의 이름을 사용
     console.log("쿠키 이름:", myCookie);
     next();
 };
